9376d60f2fdbabba979b204bb3f6b48fc5350994
Compass <ndphong165@gmail.com>
2022-11-02 21:53:47 +0700

    done springboot customer-management

---
+++ Practice/customer-management/src/main/java/com/cms/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.cms.repository;
+
+import com.cms.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ Practice/customer-management/src/main/java/com/cms/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.cms.service.customer;
+
+import com.cms.model.Customer;
+import com.cms.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
